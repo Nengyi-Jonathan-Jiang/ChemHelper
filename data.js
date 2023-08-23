3d58896572dc402a52461a8c0850c4cd7a63a69c
Jonathan Jiang <theNerdyMango@gmail.com>
2023-08-22 21:53:39 -0500

    Initial commit

---
+++ data.js
@@ -0,0 +1,52 @@
+elements = [
+    {
+        symbol: 'H',
+        name: 'Hydrogen',
+        category: 'Non-metal',
+        'atomic-mass': 1.0078,
+        // isotopes: [{
+        //     'mass-number': 1,
+        //     'atomic-mass': 1.0078,
+        //     abundance: .9998
+        // }, {
+        //     'mass-number': 2,
+        //     'atomic-mass': 2.0141,
+        //     abundance: .0002
+        // }, {
+        //     'mass-number': 3,
+        //     'atomic-mass': 3.0160,
+        //     abundance: 0
+        // }]
+    },
+    {
+        symbol: 'He',
+        name: 'Hydrogen',
+        category: 'Noble Gas',
+        'atomic-mass': 4.0026,
+        // isotopes: [{
+        //     'mass-number': 1,
+        //     'atomic-mass': 1.0078,
+        //     abundance: .9998
+        // }, {
+        //     'mass-number': 2,
+        //     'atomic-mass': 2.0141,
+        //     abundance: .0002
+        // }, {
+        //     'mass-number': 3,
+        //     'atomic-mass': 3.0160,
+        //     abundance: 0
+        // }]
+    },
+    {
+        symbol: 'Li',
+        name: 'Lithium',
+        category: 'Alkali Metal',
+        'atomic-mass': 6.9410,
+    },
+    {
+        symbol: 'Be',
+        name: 'Beryllium',
+        category: 'Alkali Earth Metal',
+        'atomic-mass': ,
+    },
+];
