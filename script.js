5ae0fb55e4cf25000aacb3a0b6a935b93b7ad27f
Jonathan Jiang <theNerdyMango@gmail.com>
2023-08-24 16:00:40 -0500

    fixed some css

--- script.js
+++ script.js
@@ -62,3 +62,36 @@ for(let i = 1; i <= elements.length; i++) {
     if([2, 10, 18, 36, 54, 86, 118].includes(i)) table_main.appendChild(createBreak());
     if(i === 70) table_bottom.appendChild(createBreak());
 }
+
+
+
+const info_bar = document.getElementById('info-bar');
+const info_name_d = document.getElementById('element-name-info');
+const info_mass_d = document.getElementById('atomic-mass-info');
+const info_o_states_d = document.getElementById('oxidation-states-info');
+
+function setCurrentElement(el) {
+    console.log(el);
+
+    if(el === null) {
+        info_bar.dataset.state = 'hidden';
+        return;
+    }
+
+    info_name_d.innerText = el.name;
+    info_mass_d.innerText = el['atomic-mass'];
+    info_o_states_d.innerText = el['oxidation-states']?.join(', ') ?? '?';
+
+    info_bar.dataset.state = '';
+}
+
+document.onclick = _ => {
+    setCurrentElement(null);
+}
+
+let allElements = document.querySelectorAll('.element');
+for(let i of allElements){
+    let idx = i.dataset.number - 1;
+    let el = elements[idx];
+    i.onclick = event => (setCurrentElement(el), event.stopPropagation());
+}
